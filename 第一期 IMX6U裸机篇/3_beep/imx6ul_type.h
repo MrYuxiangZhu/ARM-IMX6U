0b6765a9fa93f5e11832147d3ca7f6167613b696
YuxiangZHU <39479226+MrYuxiangZhu@users.noreply.github.com>
2021-04-08 23:30:19 +0800

    update

---
+++ 第一期 IMX6U裸机篇/3_beep/imx6ul_type.h
@@ -0,0 +1,34 @@
+/**************************************************************
+Copyright © zhuyuxiang. All rights reserved.
+文件名 : imx6ul_type.h
+作者 : 朱宇祥
+版本 : V1.0
+描述 : I.MX6UL开发板公共头文件
+其他 : 无
+论坛 : 
+日志 : 2021/4/6 朱宇祥创建
+**************************************************************/
+#ifndef _IMX6UL_TYPE_H
+#define _IMX6UL_TYPE_H
+
+#define __I		volatile
+#define __O		volatile
+#define __IO	volatile
+
+typedef enum
+{
+	FALSE = 0,
+	TRUE
+}BOOL;
+
+typedef unsigned char		uint8_t;
+typedef unsigned short		uint16_t;
+typedef unsigned int		uint32_t;
+typedef unsigned long long	uint64_t;
+
+typedef signed char			int8_t;
+typedef signed short		int16_t;
+typedef signed int			int32_t;
+typedef signed long long	int64_t;
+
+#endif
