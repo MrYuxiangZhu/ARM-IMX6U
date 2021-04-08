5c25848c490ef8ad2445bcd2f797df514903b4b2
YuxiangZHU <39479226+MrYuxiangZhu@users.noreply.github.com>
2021-04-08 21:51:40 +0800

    update

--- 第一期 IMX6U裸机篇/2_ledc/led.h
+++ 第一期 IMX6U裸机篇/2_ledc/led.h
@@ -11,6 +11,8 @@ Copyright © zhuyuxiang. All rights reserved.
 #ifndef _LED_H
 #define _LED_H
 
+#include "imx6ul_type.h"
+
 /*
  *LED开关控制枚举
  */
@@ -23,30 +25,30 @@ typedef enum
 /* 
  * CCM相关寄存器地址 
  */
-#define CCM_CCGR0 			*((volatile unsigned int *)0X020C4068)
-#define CCM_CCGR1 			*((volatile unsigned int *)0X020C406C)
-#define CCM_CCGR2 			*((volatile unsigned int *)0X020C4070)
-#define CCM_CCGR3 			*((volatile unsigned int *)0X020C4074)
-#define CCM_CCGR4 			*((volatile unsigned int *)0X020C4078)
-#define CCM_CCGR5 			*((volatile unsigned int *)0X020C407C)
-#define CCM_CCGR6 			*((volatile unsigned int *)0X020C4080)
+#define CCM_CCGR0 			*((__IO uint32_t *)0X020C4068)
+#define CCM_CCGR1 			*((__IO uint32_t *)0X020C406C)
+#define CCM_CCGR2 			*((__IO uint32_t *)0X020C4070)
+#define CCM_CCGR3 			*((__IO uint32_t *)0X020C4074)
+#define CCM_CCGR4 			*((__IO uint32_t *)0X020C4078)
+#define CCM_CCGR5 			*((__IO uint32_t *)0X020C407C)
+#define CCM_CCGR6 			*((__IO uint32_t *)0X020C4080)
 
 /* 
  * IOMUX相关寄存器地址 
  */
-#define SW_MUX_GPIO1_IO03 	*((volatile unsigned int *)0X020E0068)
-#define SW_PAD_GPIO1_IO03 	*((volatile unsigned int *)0X020E02F4)
+#define SW_MUX_GPIO1_IO03 	*((__IO uint32_t *)0X020E0068)
+#define SW_PAD_GPIO1_IO03 	*((__IO uint32_t *)0X020E02F4)
 
 /* 
  * GPIO1相关寄存器地址 
  */
-#define GPIO1_DR 			*((volatile unsigned int *)0X0209C000)
-#define GPIO1_GDIR 			*((volatile unsigned int *)0X0209C004)
-#define GPIO1_PSR 			*((volatile unsigned int *)0X0209C008)
-#define GPIO1_ICR1 			*((volatile unsigned int *)0X0209C00C)
-#define GPIO1_ICR2 			*((volatile unsigned int *)0X0209C010)
-#define GPIO1_IMR 			*((volatile unsigned int *)0X0209C014)
-#define GPIO1_ISR 			*((volatile unsigned int *)0X0209C018)
-#define GPIO1_EDGE_SEL 		*((volatile unsigned int *)0X0209C01C)
+#define GPIO1_DR 			*((__IO uint32_t *)0X0209C000)
+#define GPIO1_GDIR 			*((__IO uint32_t *)0X0209C004)
+#define GPIO1_PSR 			*((__IO uint32_t *)0X0209C008)
+#define GPIO1_ICR1 			*((__IO uint32_t *)0X0209C00C)
+#define GPIO1_ICR2 			*((__IO uint32_t *)0X0209C010)
+#define GPIO1_IMR 			*((__IO uint32_t *)0X0209C014)
+#define GPIO1_ISR 			*((__IO uint32_t *)0X0209C018)
+#define GPIO1_EDGE_SEL 		*((__IO uint32_t *)0X0209C01C)
 
 #endif
